6b9c52efd8447f7dc910598aa951d7e505a988cc
threeariya <144239090+threeariya@users.noreply.github.com>
2024-12-03 19:52:26 +0700

    Update setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash 
 source ~/.bashrc && \
 nvm install 18 && \
 nvm use 18 && \
-npm install -g truffle ganache
-npm install @openzeppelin/contracts
